658a01625c3ee076f08936bb1617a6f0e3cffbe4
Hazem Lenin <63303495+HazemLenin@users.noreply.github.com>
2023-05-09 20:01:53 +0200

    some changes

--- script.js
+++ script.js
@@ -1,47 +1,58 @@
 // AOS
 AOS.init({
-    disable: /bot|crawler|spider|crawling/i.test(navigator.userAgent), // Disable AOS if the visitor is a web crawler
-    once: true
+	disable: /bot|crawler|spider|crawling/i.test(navigator.userAgent), // Disable AOS if the visitor is a web crawler
+	once: true,
 });
 
 // Dark theme
-let themeBtn = document.getElementById('theme-btn');
-let sunIcon = document.getElementById('sun-icon');
-let moonIcon = document.getElementById('moon-icon');
+let themeBtn = document.getElementById("theme-btn");
+
+let sunIcon = document.getElementById("sun-icon");
+let moonIcon = document.getElementById("moon-icon");
 
 function checkTheme() {
-    // On page load or when changing themes, best to add inline in `head` to avoid FOUC
-    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-        document.documentElement.classList.add('dark');
-        if (sunIcon.classList.contains("!hidden")) {
-            sunIcon.classList.remove("!hidden");
-        }
-        if (!moonIcon.classList.contains("!hidden")) {
-            moonIcon.classList.add("!hidden");
-        }
-    } else {
-        document.documentElement.classList.remove('dark');
-        if (moonIcon.classList.contains("!hidden")) {
-            moonIcon.classList.remove("!hidden");
-        }
-        if (!sunIcon.classList.contains("!hidden")) {
-            sunIcon.classList.add("!hidden");
-        }
-    }
+	// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+	if (
+		localStorage.theme === "dark" ||
+		(!("theme" in localStorage) &&
+			window.matchMedia("(prefers-color-scheme: dark)").matches)
+	) {
+		document.documentElement.classList.add("dark"); // add dark class to html tag
+
+		// show sun icon
+		sunIcon.classList.remove("!hidden");
+
+		// hide moon icon
+		moonIcon.classList.add("!hidden");
+	} else {
+		document.documentElement.classList.remove("dark");
+
+		// remove moon icon
+		moonIcon.classList.remove("!hidden");
+
+		// hide sun icon
+		sunIcon.classList.add("!hidden");
+	}
 }
 
 checkTheme();
 
+// if you found user selected dark mode before or the device is in dark mode
+// Add dark class to html tag
+// Change icon
+// else
+// Remove dark class from html tag
+// Change icon
+
 themeBtn.addEventListener("click", () => {
-    // If sun is hidden (user pressed on moon )
-    if (sunIcon.classList.contains('!hidden')) {
-        localStorage.theme = 'dark';
-    }
-    else
-    {
-        localStorage.theme = 'light';
-    }
-    checkTheme();
+	// If sun is hidden (user pressed on moon )
+	if (sunIcon.classList.contains("!hidden")) {
+		localStorage.theme = "dark";
+	} else {
+		localStorage.theme = "light";
+	}
+
+	checkTheme();
 });
 
 // // Typing effect
@@ -77,7 +88,6 @@ themeBtn.addEventListener("click", () => {
 //     }
 // }
 
-
 // // It also uses recursion concept
 // function deleteLetter() {
 //     // We didn't delete the first letter of current skill
@@ -89,7 +99,7 @@ themeBtn.addEventListener("click", () => {
 //         setTimeout(() => deleteLetter(skills[skillsIndex]), letterDelay);
 //     } else {
 //         // We now deleted the first letter
-        
+
 //         // Set our letter index to zero
 //         letterIndex = 0;
 
@@ -109,58 +119,55 @@ themeBtn.addEventListener("click", () => {
 // typeLetter();
 
 function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+	return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 // Typing effect (simple approach)
 let typing = document.getElementById("typing");
-let letterDelay = 100;
-let wordDelay = 2000;
-let skills = [
-    "Web Developer",
-    "LOL Gamer",
-    "Rock fan",
-    "Marvel fan"
-];
+
+let letterDelay = 100; // time between letters
+let wordDelay = 2000; // time between words
+
+let skills = ["Web Developer", "LOL Gamer", "Rock fan", "Marvel fan"];
 
 // We want an infinite animation
 async function type() {
-    while (true) {
-        // Go through skills
-        for (let skill of skills) {
-            await typeSkill(skill);
-            await sleep(wordDelay);
-            await deleteSkill(skill);
-        }
-    }
+	while (true) {
+		// // Go through skills
+		for (let skill of skills) {
+			await typeSkill(skill);
+
+			await sleep(wordDelay);
+
+			await deleteSkill(skill);
+		}
+	}
 }
 
 async function typeSkill(skill) {
-    for (let char of skill) {
-        typing.innerHTML += char;
-        console.log(typing.innerHTML);
-        await sleep(letterDelay);
-    }
+	for (let char of skill) {
+		typing.innerHTML += char; // Add letter
+		await sleep(letterDelay);
+	}
 }
 
 async function deleteSkill(skill) {
-    for (let char of skill) {
-        typing.innerHTML = typing.innerHTML.slice(0, -1);
-        console.log(typing.innerHTML);
-        await sleep(letterDelay);
-    }
+	for (let char of skill) {
+		typing.innerHTML = typing.innerHTML.slice(0, -1); // Removes last char
+		await sleep(letterDelay);
+	}
 }
 
 type();
 
 // Carousel
-$('.carousel').slick({
-    dots: false,
-    infinite: true,
-    accessibility: false,
-    mobileFirst: true,
-    centerMode: true,
-    variableWidth: true,
-    prevArrow: $('#prev-btn'),
-    nextArrow: $('#next-btn')
+$(".carousel").slick({
+	dots: false,
+	infinite: true,
+	accessibility: false,
+	mobileFirst: true,
+	centerMode: true,
+	variableWidth: true,
+	prevArrow: $("#prev-btn"),
+	nextArrow: $("#next-btn"),
 });
